1508c14c2650d279075b858829dde23a26e0e93a
wrmungas <wrmungas@gmail.com>
2026-03-04 16:21:51 -0500

    [Int] rename gfx.h

--- include/wrm/graphics.h
+++ include/wrm/graphics.h
@@ -36,37 +36,39 @@ REQUIREMENTS:
 /* --- Type Declarations --------------------------------------------------- */
 
 // render module settings
-typedef struct wrm_gfx_Settings wrm_gfx_Settings;
+typedef struct gfx_Settings 
+gfx_Settings;
 // window creation arguments
-typedef struct wrm_gfx_Window_Info wrm_gfx_Window_Info; 
-// data format for meshes and shaders
-typedef struct wrm_gfx_Format wrm_gfx_Format;
+typedef struct gfx_Window_Info 
+gfx_Window_Info; 
+// data format shared between meshes and shaders
+typedef struct gfx_Format 
+gfx_Format;
 // single 32-bit integer rgba value
-typedef u32 wrm_gfx_RGBAi;
+typedef u32 rgba32;
 // struct of 4 bytes: r, g, b, a
-typedef struct wrm_gfx_RGBA wrm_gfx_RGBA;
+typedef struct gfx_RGBA 
+gfx_RGBA;
 // struct of 4 floats: r, g, b, a: used for when OpenGL wants color channel 
 // values as floats in the range [0.0f, 1.0f]
-typedef struct wrm_gfx_RGBAf wrm_gfx_RGBAf;
+typedef struct gfx_RGBAf 
+gfx_RGBAf;
 // data for creating a shader
-typedef struct wrm_gfx_Shader_Info wrm_gfx_Shader_Info;
+typedef struct gfx_Shader_Info 
+gfx_Shader_Info;
 // data for creating a texture
-typedef struct wrm_gfx_Texture_Info wrm_gfx_Texture_Info;
+typedef struct gfx_Texture_Info 
+gfx_Texture_Info;
 // Arguments for mesh creation
-typedef struct wrm_gfx_Mesh_Info wrm_gfx_Mesh_Info;
+typedef struct wgfx_Mesh_Info 
+gfx_Mesh_Info;
 // Arguments for model creation
-typedef struct wrm_gfx_Model_Info wrm_gfx_Model_Info;
+typedef struct wrm_Model_Info 
+wrm_Model_Info;
 
 /* --- Type definitions ---------------------------------------------------- */
 
-struct wrm_gfx_Settings {
-    bool verbose;
-    bool test;
-    bool errors;
-    char *shaders_dir;
-};
-
-struct wrm_gfx_Window_Info {
+struct wrm_Window_Info {
     const char *name; // the name of the window
     i32 height_px; // the height of the window in pixels
     i32 width_px; // the width of the window in pixels
@@ -82,27 +84,27 @@ struct wrm_gfx_Format { // TODO add normals, material properties, etc
     // shaders ALWAYS take position, and meshes MUST provide it
 };
 
-struct wrm_gfx_RGBA {
+struct wrm_RGBA {
     u8 r;
     u8 g;
     u8 b;
     u8 a;
 };
 
-struct wrm_gfx_RGBAf {
+struct wrm_RGBAf {
     float r;
     float g;
     float b;
     float a;
 };
 
-struct wrm_gfx_Shader_Info {
-    wrm_gfx_Format format;
+struct wrm_Shader_Info {
+    wrm_GL_Format format;
     char *vert;
     char *frag; 
 };
 
-struct wrm_gfx_Texture_Info {
+struct wrm_Texture_Info {
     u8 *pixels; // array of 4 x width * height bytes (or NULL to fill later)
     u32 width; // width of the texture, in pixels
     u32 height; // height of the texture, in pixels
@@ -110,7 +112,7 @@ struct wrm_gfx_Texture_Info {
     bool transparent;
 };
 
-struct wrm_gfx_Mesh_Info {
+struct wrm_Mesh_Info {
     wrm_gfx_Format format; // data format of the mesh: must match shader used
     float *positions;       // position for each vertex
     float *colors;          // RGBA color for each vertex
@@ -124,7 +126,7 @@ struct wrm_gfx_Mesh_Info {
     bool transparent;       // if the mesh has colors, is the alpha anything other than 1 ?
 };
 
-struct wrm_gfx_Model_Info { 
+struct wrm_Model_Info { 
     // these are relative to the parent model
     vec3 pos;
     vec3 rot;
@@ -149,27 +151,27 @@ extern const u32 WRM_DEFAULT_WINDOW_HEIGHT;
 
 // bit masks for colors 
 
-#define WRM_RGBAi_R_BITS 0xff000000u
-#define WRM_RGBAi_G_BITS 0x00ff0000u
-#define WRM_RGBAi_B_BITS 0x0000ff00u
-#define WRM_RGBAi_A_BITS 0x000000ffu
+#define WRM_RGBA32_R_BITS 0xff000000u
+#define WRM_RGBA32_G_BITS 0x00ff0000u
+#define WRM_RGBA32_B_BITS 0x0000ff00u
+#define WRM_RGBA32_A_BITS 0x000000ffu
 
 // a few simple predefined colors
 
-#define WRM_RGBA_BLACK 0x000000ffu
-#define WRM_RGBA_WHITE 0xffffffffu
+#define WRM_RGBA32_BLACK 0x000000ffu
+#define WRM_RGBA32_WHITE 0xffffffffu
 
-#define WRM_RGBA_RED 0xff0000ffu
-#define WRM_RGBA_GREEN 0x00ff00ffu
-#define WRM_RGBA_BLUE 0x0000ffffu
+#define WRM_RGBA32_RED 0xff0000ffu
+#define WRM_RGBA32_GREEN 0x00ff00ffu
+#define WRM_RGBA32_BLUE 0x0000ffffu
 
-#define WRM_RGBA_PURPLE 0xff00ffffu
-#define WRM_RGBA_YELLOW 0xffff00ffu
-#define WRM_RGBA_CYAN 0x00ffffffu
+#define WRM_RGBA32_PURPLE 0xff00ffffu
+#define WRM_RGBA32_YELLOW 0xffff00ffu
+#define WRM_RGBA32_CYAN 0x00ffffffu
 
 /* --- Module functions ---------------------------------------------------- */
 
-// --- GENERAL ---
+// --- GRAPHICS MODULE ---
 
 /* 
 Initialize the renderer - opens a window with a GL context, 
@@ -198,67 +200,67 @@ void wrm_gfx_debugFrame(void);
 // --- SHADER ---
 
 /* Creates a shader program using the given shader source strings */
-bool wrm_gfx_createShader( 
+bool wrm_createShader( 
     wrm_gfx_Shader_Info *args,
     wrm_Ref *dest
 );
 /* For debugging; prints a shader's data to `stdout` */
-void wrm_gfx_debugShader(wrm_Ref shader);
+void wrm_debugShader(wrm_Ref shader);
 /* 
 Removes a shader and its associated resources 
 Called internally when shader creation fails and by wrm_gfx_quit() to free all 
 render resources
 As long as wrm_gfx_quit() is called this need not be
 */
-void wrm_gfx_deleteShader(wrm_Ref shader);
+void wrm_deleteShader(wrm_Ref shader);
 
 // --- TEXTURE ---
 
 /* Creates a texture */
-bool wrm_gfx_createTexture(const wrm_gfx_Texture_Info *args, wrm_Ref *dest);
+bool wrm_createTexture(const wrm_gfx_Texture_Info *args, wrm_Ref *dest);
 /* 
 Update the data of texture, offset from x and y, with the provided pixel
 data 
 */
-bool wrm_gfx_updateTexture(
+bool wrm_updateTexture(
     wrm_Ref texture, 
     wrm_gfx_Texture_Info *data, 
     u32 x, 
     u32 y
 );
 /* For debugging; prints a stexture's data to `stdout` */
-void wrm_gfx_debugTexture(wrm_Ref texture);
+void wrm_debugTexture(wrm_Ref texture);
 /* 
 Removes a texture and its associated resources 
 Called internally when texture creation fails and by wrm_gfx_quit() to free all
 render resources
 As long as wrm_gfx_quit() is called this need not be
 */
-void wrm_gfx_deleteTexture(wrm_Ref texture);
+void wrm_deleteTexture(wrm_Ref texture);
 
 // --- MESH ---
 
 /* Create a mesh */
-bool wrm_gfx_createMesh(const wrm_gfx_Mesh_Info *args, wrm_Ref *dest);
+bool wrm_createMesh(const wrm_gfx_Mesh_Info *args, wrm_Ref *dest);
 /* 
 Clones an existing mesh (useful for changing the mesh of a single entity using
 the model without affecting all others
 */
-bool wrm_gfx_cloneMesh(wrm_Ref src, wrm_Ref *dest);
+bool wrm_cloneMesh(wrm_Ref src, wrm_Ref *dest);
 /* 
 Updates a mesh's data: IMPORTANT: ALL models using this mesh will now use 
 the updated version 
 */
-bool wrm_gfx_updateMesh(wrm_Ref mesh, const wrm_gfx_Mesh_Info *data);
+bool wrm_updateMesh(wrm_Ref mesh, const wrm_gfx_Mesh_Info *data);
 /* For debugging; prints a mesh's data to `stdout` */
-void wrm_gfx_debugMesh(wrm_Ref mesh);
+void wrm_debugMesh(wrm_Ref mesh);
 /* 
 Removes a mesh and its associated resources 
 Called internally when mesh creation fails and by wrm_gfx_quit() to free all 
 render resources
 As long as wrm_gfx_quit() is called this need not be called directly
 */
-void wrm_gfx_deleteMesh(wrm_Ref mesh);
+void wrm_deleteMesh(wrm_Ref mesh);
 
 // --- MODEL ---
 
@@ -266,7 +268,7 @@ void wrm_gfx_deleteMesh(wrm_Ref mesh);
 Create a model - if use_default_shader is true, the renderer will attempt to 
 select a default shader based on the mesh attributes 
 */
-bool wrm_gfx_createModel(
+bool wrm_createModel(
     const wrm_gfx_Model_Info *data, 
     wrm_Ref *parent, 
     bool use_default_shader
@@ -275,7 +277,7 @@ bool wrm_gfx_createModel(
 Sets the given model's transform to the argument values
 Ignores any NULL arguments 
 */
-bool wrm_gfx_setModelTransform(
+bool wrm_setModelTransform(
     wrm_Ref model, 
     const vec3 pos, 
     const vec3 rot, 
@@ -285,32 +287,32 @@ bool wrm_gfx_setModelTransform(
 Adds the argument values to the given model's transform, 
 ignoring any NULL arguments 
 */
-bool wrm_gfx_addModelTransform(
+bool wrm_addModelTransform(
     wrm_Ref model, 
     const vec3 pos, 
     const vec3 rot, 
     const vec3 scale
 );
 /* Set a model's mesh */
-bool wrm_gfx_setModelMesh(wrm_Ref model, wrm_Ref mesh);
+bool wrm_setModelMesh(wrm_Ref model, wrm_Ref mesh);
 /* Set a model's texture*/
-bool wrm_gfx_setModelTexture(wrm_Ref model, wrm_Ref texture);
+bool wrm_setModelTexture(wrm_Ref model, wrm_Ref texture);
 /* Set a model's shader - checks for compatibility with the model's mesh */
-bool wrm_gfx_setModelShader(wrm_Ref model, wrm_Ref shader);
+bool wrm_setModelShader(wrm_Ref model, wrm_Ref shader);
 /* Toggle model visibility */
-void wrm_gfx_setModelShown(wrm_Ref model, bool shown);
+void wrm_setModelShown(wrm_Ref model, bool shown);
 /* Toggle visibility of model's children */
-void wrm_gfx_setChildrenShown(wrm_Ref model, bool shown);
+void wrm_setChildrenShown(wrm_Ref model, bool shown);
 /* Associates models `child` and `parent` as such */
-bool wrm_gfx_addChild(wrm_Ref parent, wrm_Ref child);
+bool wrm_addChild(wrm_Ref parent, wrm_Ref child);
 /* Orphans model `child` from `parent` */
-bool wrm_gfx_removeChild(wrm_Ref parent, wrm_Ref child);
+bool wrm_removeChild(wrm_Ref parent, wrm_Ref child);
 /* creates a default colored test triangle - for testing */
-bool wrm_gfx_createTestTriangle(wrm_Ref *dest);
+bool wrm_createTestTriangle(wrm_Ref *dest);
 /* creates a default error-textured test cube - for testing */
-bool wrm_gfx_createTestCube(wrm_Ref *dest);
+bool wrm_createTestCube(wrm_Ref *dest);
 /* For debugging; prints a model's data to `stdout` */
-void wrm_gfx_debugModel(wrm_Ref model);
+void wrm_debugModel(wrm_Ref model);
 /* 
 Removes a model (BUT NOT its resources - these may be in use by other models)
 Called internally when model creation fails and by wrm_gfx_quit() to free all 
@@ -350,7 +352,7 @@ inline wrm_gfx_RGBAi wrm_RGBAi_fromRGBA(wrm_gfx_RGBA rgba)
     | (u32)rgba.a;
 }
 
-inline wrm_gfx_RGBAi wrm_RGBA_fromRGBAf(wrm_gfx_RGBAf rgbaf)
+inline wrm_gfx_RGBAi wrm_RGBAi_fromRGBAf(wrm_gfx_RGBAf rgbaf)
 {
     return (u32)(rgbaf.r * 255.0) << 24 
     | (u32)(rgbaf.g * 255.0) << 24 
