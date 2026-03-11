52d07149f69fc64a909824fa67aabed37781d7a7
wrmungas <wrmungas@gmail.com>
2026-03-11 16:32:16 -0400

    [T] outline test structure for memory

--- test/test-memory.c
+++ test/test-memory.c
@@ -1,21 +1,43 @@
 #include "../src/wrm/memory/memory.h"
 #include "test.h"
+#include <stdlib.h>
 
-wrm_UNIT(test_init) 
+//--- SETUP -------------------------------------------------------------------
+
+// setup each test
+void before_each()
+{
+    mem_init(realloc);
+}
+
+// shutdown each test
+void after_each()
+{
+
+}
+
+//--- TESTS -------------------------------------------------------------------
+
+wrm_TESTDEF(test_init)
+{
+    if(id_counter != 1) { wrm_FAIL(); }
+    if(internal_resize != realloc) { wrm_FAIL(); }
+
+    wrm_PASS();
+}
+
+wrm_TESTDEF(test_init_pool) 
 {
     return true;
 }
 
-wrm_UNIT(test_init_pool) 
-{
-    return true;
-}
 
+//--- RUN ---------------------------------------------------------------------
 
-wrm_Test tests[] = {
+wrm_Test to_run[] = {
     wrm_TEST(test_init_pool)
 };
 
-wrm_RUN("MEMORY", tests);
+wrm_RUN("MEMORY", to_run, before_each, after_each);
 
 
