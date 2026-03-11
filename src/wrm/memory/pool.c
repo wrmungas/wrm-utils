6ac4aa081090b97940dc791a2d534960702d9c6e
wrmungas <wrmungas@gmail.com>
2026-03-11 13:49:12 -0400

    [I] Implement pool functions

--- src/wrm/memory/pool.c
+++ src/wrm/memory/pool.c
@@ -1,115 +1,137 @@
-#include "wrm/memory.h"
+#include "memory.h"
 
-
-bool wrm_Pool_init(wrm_Pool *p, size_t cap, size_t element_size, bool auto_reserve)
+// gets a pointer to a slot in the pool
+inline void *get_slot(mem_Pool *p, u32 idx) 
 {
-    if(!p) return false;
-
-    p->cap = cap;
-    p->e_size = element_size;
-    p->used_cnt = 0;
-
-    p->data = calloc(cap, element_size);
-    p->in_use = calloc(cap, sizeof(bool));
-    p->auto_reserve = auto_reserve;
-
-    return p->data && p->in_use;
+    return (void*)((u8*)(p->data) + p->item_size * idx);
 }
 
-wrm_Option_Handle wrm_Pool_getSlot(wrm_Pool *p)
+/*
+Gets the 'next' value (index of the next free slot) stored at a slot in
+the pool (assumes the slot is a free slot)
+*/
+inline u32 get_next(mem_Pool *p, u32 idx) 
 {
-    if(p->used_cnt == p->cap) {
-        if(!(p->auto_reserve && wrm_Pool_reserve(p, p->cap * WRM_MEMORY_GROWTH_FACTOR))) {
-            return OPTION_NONE(Handle);
+    return *((u32*)get_slot(p, idx));
+}
+/* Checks whether a slot in the pool is free */
+inline bool is_free(mem_Pool *p, u32 idx)
+{
+    u32 this = p->free_head;
+    while(this < p->item_cap) {
+        if(this == idx) {
+            return true;
         }
-        p->in_use[p->used_cnt] = true;
-        memset(wrm_Pool_at(p, p->used_cnt), 0, p->e_size); // clear any prior data to zero
-        return OPTION_SOME(Handle, p->used_cnt++);
+        this = get_next(p, this);
     }
-
-    size_t i = 0;
-    while(p->in_use[i]) { i++; }
-    p->in_use[i] = true;
-    p->used_cnt++;
-    memset(wrm_Pool_at(p, i), 0, p->e_size);
-    return OPTION_SOME(Handle, i);
+    return false;
 }
 
-bool wrm_Pool_reserve(wrm_Pool *p, size_t capacity)
+/*
+Sets the 'next' value (index of the next free slot) stored at a slot in
+the pool (assumes the slot is a free slot)
+*/
+inline void set_next(mem_Pool *p, u32 idx, u32 next) 
 {
-    if(capacity >= WRM_POOL_MAX_CAPACITY) return false;
-    // reallocate
-    void *temp = realloc(p->data, capacity * p->e_size );
-    if(!temp) { return false; }
-    p->data = temp;
-
-    temp = realloc(p->in_use, capacity * sizeof(bool));
-    if(!temp ) { return false; }
-    p->in_use = temp;
-    
-    p->cap = capacity;
-    return true;
+    *((u32*)get_slot(p, idx)) = next;
 }
 
-bool wrm_Pool_shrink(wrm_Pool *p, size_t capacity)
-{
-    if(capacity < p->used_cnt) return false;
-
-    // allocate a new, smaller pool
-    wrm_Pool new_pool;
-    wrm_Pool_init(&new_pool, capacity, p->e_size, p->auto_reserve);
-
-    // copy (shallow) all the old elements over
-    for(u32 i = 0; i < p->cap; i++) {
-        if(p->in_use[i]) {
-            void *src = wrm_Pool_at(p, i); 
-            wrm_Option_Handle result = wrm_Pool_getSlot(&new_pool);
-            if(!result.exists) {
-                wrm_error("Pool", "shrink()", "could not copy all data from the old to the new buffer!!");
-                return false;
-            }
-            void *dest = wrm_Pool_at(&new_pool, result.val);
-            memcpy(dest, src, p->e_size);
-        }
-    }
-
-    // just to be sure
-    if(new_pool.used_cnt != p->used_cnt) {
-        wrm_error("Pool", "shrink()", "could not copy all data from the old to the new buffer!!");
+bool mem_init_pool(
+    mem_Pool *p, 
+    void *buf, 
+    size_t isize, 
+    size_t icap,
+    bool final
+) {
+    if(!id_counter) {
         return false;
     }
 
-    // delete (shallow) old resources and swap
-    wrm_Pool_delete(p, NULL);
-    *p = new_pool;
-    return true;
+    *p = (mem_Pool) {
+        .data = buf,
+        .id = id_counter++,
+        .item_cap = icap,
+        .item_size = isize,
+        .free_cnt = icap,
+        .free_head = 0,
+        .final = final        
+    };
+
+    // initialize free list - indices beyond p->item_cap are considered invalid
+    for(u32 i = 0; i < icap; i++) {
+        set_next(p, i, i + 1);
+    }
 }
 
-void wrm_Pool_delete(wrm_Pool *p, void (*delete)(void *element))
+mem_Ref mem_palloc(mem_Pool *p)
 {
-    if(!p || !p->data || !p->in_use) { return; }
+    if(!p) { return mem_null; }
+    if(p->free_cnt == 0) { 
+        if(p->final || !mem_resize) {
+            return mem_null;
+        }
+        void *new_data = mem_resize(
+            p->data, 
+            p->item_cap * MEM_GROWTH_FACTOR * p->item_size 
+        );
 
-    if(delete) {
-        for(u32 i = 0; i < p->cap; i++) {
-            if(p->in_use[i]) delete(wrm_Pool_at(p, i)); 
+        if(!new_data) {
+            return mem_null;
+        }
+
+        p->data = new_data;
+        size_t old_cap = p->item_cap;
+        p->item_cap *= MEM_GROWTH_FACTOR;
+        p->free_cnt += p->item_cap - old_cap;
+    }
+
+    mem_Ref ret = (mem_Ref){p->id, p->free_head};
+
+    u32 *head = get_slot(p, p->free_head);
+    p->free_head = *head;
+    p->free_cnt--;
+
+    return ret;
+}
+
+void *mem_pderef(mem_Pool *p, mem_Ref r)
+{
+    if(!p || !r.src || r.src != p->id) { return NULL; }
+
+    return get_slot(p, r.idx);
+}
+
+void mem_pfree(mem_Pool *p, mem_Ref *r, void (*delete)(void *))
+{
+    if(!p || !r || !r->src || r->src != p->id) {
+        return;
+    }
+    u32 *slot = get_slot(p, r->idx);
+    if(delete) { delete(slot); }
+
+    *slot = p->free_head;
+    p->free_head = r->idx;
+    p->free_cnt++;
+
+    *r = mem_null;
+}
+
+void mem_delete_pool(
+    mem_Pool *p, 
+    wrm_FUNC(delete, void, void*), 
+    wrm_FUNC(free, void, void*)
+) {
+    if(!p || !p->id) {
+        return;
+    }
+
+    // call delete on all slots currently in-use (this might be tough, or at least slow)
+    for(u32 i = 0; i < p->item_cap; i++) {
+        if(!is_free(p, i) && delete) {
+            delete(get_slot(p, i));
         }
     }
-    
 
-    free(p->data);
-    free(p->in_use);
-
-    p->data = NULL;
-    p->in_use = NULL;
-
-    p->used_cnt = 0;
-    p->e_size = 0;
-    p->cap = 0;
+    p->id = 0;
+    if(free) { free(p->data); }
 }
-
-// force the compiler to emit a symbol for these
-
-bool wrm_Pool_isValid(wrm_Pool *p, wrm_Handle idx);
-void wrm_Pool_freeSlot(wrm_Pool *p, wrm_Handle idx);
-void *wrm_Pool_offsetAt(wrm_Pool *p, wrm_Handle idx, size_t offset);
-void *wrm_Pool_at(wrm_Pool *p, wrm_Handle idx);
